7943b6936624a998e580991c6e30af35a00b289e
achaplygin <hairesiss@gmail.com>
2018-05-03 15:16:21 +0300

    Create new Activity to create new user

---
+++ app/src/main/java/com/example/che/alex/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.example.che.alex.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createUserChooseColorClick(view: View) {
+
+    }
+
+    fun createUserCreateClick(view: View) {
+
+    }
+
+    fun avatarImageChooseClick(view: View) {
+
+    }
+}
